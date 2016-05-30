cc28fecc34a271fee056b548a7226ea79d2f89c9
Chris Myers <7chrismmyers7@gmail.com>
2016-05-30 13:37:49 -0600

    Completed mini-Routing project

---
+++ js/app.js
@@ -0,0 +1,22 @@
+angular.module('miniRouting', ['ui.router'])
+    .config(function($stateProvider) {
+    
+    $stateProvider
+    
+        .state('home', {
+            url: '/',
+            templateUrl: 'js/home/home.html',
+            controller: 'homeCtrl'
+    })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'js/settings/settingsTmpl.html',
+            controller: 'settingsCtrl'
+    })
+        .state('products', {
+            url: '/products/:id',
+            templateUrl: 'js/products/productTmpl.html',
+            controller: 'productsCtrl'
+    })
+    
+})
